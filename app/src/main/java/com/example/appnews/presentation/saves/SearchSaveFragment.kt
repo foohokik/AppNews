8a1cb5d5a9cd6da9dfa1986c0812d0f70d10618e
Dmitry Gabrusevich <foohokik@yandex.ru>
2024-02-25 12:48:11 +0300

    fullarticleheadlinesfragment_savefragment

---
+++ app/src/main/java/com/example/appnews/presentation/saves/SearchSaveFragment.kt
@@ -0,0 +1,41 @@
+package com.example.appnews.presentation.saves
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.appnews.R
+import com.example.appnews.databinding.FragmentSaveBinding
+import com.example.appnews.databinding.FragmentSearchSaveBinding
+
+class SearchSaveFragment : Fragment() {
+
+    private var _binding: FragmentSearchSaveBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentSearchSaveBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
