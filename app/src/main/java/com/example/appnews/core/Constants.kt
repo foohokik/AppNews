7cefbb1b72a7adb73d9460f30e9af7ca2bc1490f
pavel gabrusevich <Gabrusevich_P_E@itgri.ru>
2024-04-07 12:59:20 +0400

    убрал лишнее

--- app/src/main/java/com/example/appnews/core/Constants.kt
+++ app/src/main/java/com/example/appnews/core/Constants.kt
@@ -1,19 +1,3 @@
 package com.example.appnews.core
 
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-
 const val PAGE_SIZE = 20
-
-fun<T> Call<T>.onEnqueue(actOnSuccess: (Response<T>) -> Unit, actOnFailure: (t: Throwable?) -> Unit)   {
-    this.enqueue(object: Callback<T> {
-        override fun onResponse(call: Call<T>, response: Response<T>) {
-            actOnSuccess(response)
-        }
-
-        override fun onFailure(call: Call<T>, t: Throwable) {
-            actOnFailure(t)
-        }
-    })
-}
